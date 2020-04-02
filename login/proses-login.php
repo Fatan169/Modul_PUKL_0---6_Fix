501427713d34548f911f3e5b788178bddaa2c5cd
Fatan Virgian Nova <53498573+Fatan169@users.noreply.github.com>
2020-04-02 21:03:43 +0700

    Add files via upload

---
+++ login/proses-login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include '../aset/koneksi.php';
+
+if(isset($_POST['btnlogin']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id_petugas, nama_petugas FROM petugas 
+            WHERE username='$username' AND password='$password'";
+    $res = mysqli_query($kon, $sql);
+
+    $count = mysqli_affected_rows($kon);
+
+    if($count == 1)
+    {
+        $data_login = mysqli_fetch_assoc($res);
+        
+        $_SESSION['id_petugas'] = $data_login['id_petugas']; 
+       
+        
+        $_SESSION['nama_petugas'] = $data_login['nama']; 
+        
+
+        header("Location: http://localhost/siperpus/aset/index.php");
+    }
+    else
+    {   
+        header("Location: http://localhost/siperpus/login/index.php");
+    }
+}
+?>
